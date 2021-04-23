d1610a05c8b53e13bccd5638499d516b02dca2fe
Tsai <abla00@gmail.com>
2021-04-23 17:26:05 +0800

    Get /health_check

---
+++ app/controllers/health_checks_controller.rb
@@ -0,0 +1,5 @@
+class HealthChecksController < ApplicationController
+  def show
+    head 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles
+  get '/health_check', to: 'health_checks#show'
 end
